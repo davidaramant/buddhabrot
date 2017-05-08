3ad0209191834ef18828e5ee71aef8f73cc3e3b6
David Aramant <aramant@sep.com>
2017-05-08 15:16:36 -0400

    NoResults singleton

---
+++ src/Buddhabrot/IterationKernels/NoResults.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buddhabrot.Core;
+
+namespace Buddhabrot.IterationKernels
+{
+    sealed class NoResults : IPointBatchResults
+    {
+        public static readonly IPointBatchResults Instance = new NoResults();
+
+        public int Count => 0;
+        public FComplex GetPoint(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIteration(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        private NoResults()
+        {
+            
+        }
+    }
+}

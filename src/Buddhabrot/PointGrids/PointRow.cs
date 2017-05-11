56bce714cb188c2f9d618e9ec22fc3c810ee0a29
David Aramant <aramant@sep.com>
2017-05-11 15:44:14 -0400

    More ways to get data out of PointRow

--- src/Buddhabrot/PointGrids/PointRow.cs
+++ src/Buddhabrot/PointGrids/PointRow.cs
@@ -4,13 +4,31 @@ using System.Linq;
 
 namespace Buddhabrot.PointGrids
 {
+    /// <summary>
+    /// Stores whether the points in the row are inside the Mandelbrot set.
+    /// </summary>
     public sealed class PointRow : IEnumerable<bool>
     {
-        private readonly List<(bool inSet, int start, int length)> _segments = 
+        private readonly List<(bool inSet, int start, int length)> _segments =
             new List<(bool inSet, int start, int length)>();
 
         public int Y { get; }
         public int Width { get; }
+
+        public bool this[int pointIndex]
+        {
+            get
+            {
+                var indexOfSegmentPastPointIndex = _segments.FindIndex(s => s.start > pointIndex);
+
+                var segment =
+                    indexOfSegmentPastPointIndex == -1
+                    ? _segments.Last()
+                    : _segments[indexOfSegmentPastPointIndex - 1];
+
+                return segment.inSet;
+            }
+        }
 
         public PointRow(int width, int y, IEnumerable<(bool inSet, int length)> segments)
         {
@@ -20,13 +38,25 @@ namespace Buddhabrot.PointGrids
             int position = 0;
             foreach (var segment in segments)
             {
-                _segments.Add((segment.inSet,position,segment.length));
+                _segments.Add((segment.inSet, position, segment.length));
                 position += segment.length;
             }
 
             if (position != Width)
             {
-                throw new ArgumentException("The segments do not add up to a full row.");
+                throw new ArgumentException($"The segments should have added up to {width} but reached {position}.");
+            }
+        }
+
+        public IEnumerable<int> GetXPositionsOfSetEdges()
+        {
+            foreach (var segment in _segments.Where(s => s.inSet))
+            {
+                yield return segment.start;
+                if (segment.length > 1)
+                {
+                    yield return segment.start + segment.length - 1;
+                }
             }
         }
 
---
+++ src/Tests/PointGrids/PointRowTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Buddhabrot.PointGrids;
+using NUnit.Framework;
+
+namespace Tests.PointGrids
+{
+    [TestFixture]
+    public sealed class PointRowTests
+    {
+        [Test]
+        public void ShouldReturnEdgesOfSet()
+        {
+            //0123456789
+            //  SSS SSSS
+
+            var row = new PointRow(width: 10, y: 0, segments: new[]
+            {
+                (false,2),
+                (true,3),
+                (false,1),
+                (true,4),
+            });
+
+            var edgeLocations = row.GetXPositionsOfSetEdges().ToArray();
+
+            Assert.That(
+                edgeLocations,
+                Is.EqualTo(new[] { 2, 4, 6, 9 }),
+                "Did not return set edges.");
+        }
+
+        [Test]
+        public void ShouldReturnCorrectValuesFromIndexer()
+        {
+            //0123456789
+            //  SSS SSSS
+
+            var row = new PointRow(width: 10, y: 0, segments: new[]
+            {
+                (false,2),
+                (true,3),
+                (false,1),
+                (true,4),
+            });
+
+            var expected = row.ToArray();
+
+            for (int i = 0; i < row.Width; i++)
+            {
+                Assert.That(row[i], Is.EqualTo(expected[i]), $"Unexpected value at index {i}");
+            }
+        }
+    }
+}

482b051442ea0b6699e22fb6315abb590c696de3
David Aramant <aramant@sep.com>
2017-05-12 11:44:08 -0400

    Float versions of Complex, ComplexArea, and Range

---
+++ src/Buddhabrot/Core/FComplex.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Buddhabrot.Core
+{
+    /// <summary>
+    /// A float version of <see cref="System.Numerics.Complex"/>
+    /// </summary>
+    public struct FComplex : IEquatable<FComplex>
+    {
+        public readonly float Real;
+        public readonly float Imaginary;
+
+        public FComplex(float real, float imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        // Match the format for Complex for consistency.
+        public override string ToString() => $"({Real}, {Imaginary})";
+
+        public float MagnitudeSquared() => Real * Real + Imaginary * Imaginary;
+
+        #region Equality members
+
+        public bool Equals(FComplex other)
+        {
+            return Real.Equals(other.Real) && Imaginary.Equals(other.Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is FComplex && Equals((FComplex)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Real.GetHashCode() * 397) ^ Imaginary.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(FComplex left, FComplex right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(FComplex left, FComplex right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion Equality members
+    }
+}

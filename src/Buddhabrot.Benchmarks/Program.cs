9440dbba96b54ddbd012cd15a286047a838506c1
David Aramant <aramant@sep.com>
2017-05-16 17:57:52 -0400

    WIP on benchmark for BorderPoint calculation

--- src/Buddhabrot.Benchmarks/Program.cs
+++ src/Buddhabrot.Benchmarks/Program.cs
@@ -1,4 +1,7 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Running;
+using Humanizer;
 
 namespace Buddhabrot.Benchmarks
 {
@@ -6,7 +9,33 @@ namespace Buddhabrot.Benchmarks
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<AutomaticIterationLimitBenchmarks>();
+            Console.Out.WriteLine(BorderPointBenchmarks.Span.Length());
+            Console.Out.WriteLine(BorderPointBenchmarks.Span.Length() / ((long)1 << 38));
+
+
+            var bpb = new BorderPointBenchmarks();
+
+            //SimpleBenchmark(bpb.FindBorderPoint, "Find Border Point");
+            SimpleBenchmark(bpb.FindBorderPointsMagnitudeSquared, "Find Border Point Mag Squared");
+
+            // BenchmarkRunner.Run<AutomaticIterationLimitBenchmarks>();
+            //BenchmarkRunner.Run<BorderPointBenchmarks>();
+        }
+
+        private static void SimpleBenchmark<T>(Func<T> method, string name)
+        {
+            Console.WriteLine($"{new string('-', 20)}\n{name}\nResult: {method()}");
+            GC.Collect();
+            const int trials = 3;
+            var timer = Stopwatch.StartNew();
+
+            for (int i = -0; i < trials; i++)
+            {
+                GC.KeepAlive(method());
+            }
+
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed.Humanize(2));
         }
     }
 }

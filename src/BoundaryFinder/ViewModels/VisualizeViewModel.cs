47939abd0ab4c66b2885919fe28dc64e13767ce2
David Aramant <davidaramant@gmail.com>
2022-08-08 22:15:45 -0400

    WIP on VisualizeViewModel

--- src/BoundaryFinder/ViewModels/VisualizeViewModel.cs
+++ src/BoundaryFinder/ViewModels/VisualizeViewModel.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections.ObjectModel;
 using BoundaryFinder.Models;
 using Buddhabrot.Core.Boundary;
+using ReactiveUI;
 
 namespace BoundaryFinder.ViewModels;
 
 public sealed class VisualizeViewModel : ViewModelBase
 {
     private readonly BorderDataProvider _dataProvider;
+    private BoundaryParameters? _selectedParameters;
+    private readonly ObservableAsPropertyHelper<int> _minimumIterationsCap;
+    private int _minimumIterations = 0;
 
     public ObservableCollection<BoundaryParameters> SavedBoundaries => _dataProvider.SavedBoundaries;
 
+    public BoundaryParameters? SelectedParameters
+    {
+        get => _selectedParameters;
+        set => this.RaiseAndSetIfChanged(ref _selectedParameters, value);
+    }
+
+    public int MinimumIterationsCap => _minimumIterationsCap.Value;
+    
+    public int MinimumIterations
+    {
+        get => _minimumIterations;
+        set => this.RaiseAndSetIfChanged(ref _minimumIterations, value);
+    }
+    
     public VisualizeViewModel(BorderDataProvider dataProvider)
     {
         _dataProvider = dataProvider;
         
-        // TODO: Expose MaximumIterations; bind the MinimumIterations spinner Max to it
+        this.WhenAnyValue(x => x.SelectedParameters, 
+                bp => bp?.MaxIterations - 1 ?? 0)
+            .ToProperty(this, x => x.MinimumIterationsCap, out _minimumIterationsCap);
     }
 }

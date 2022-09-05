0d52cacbb329260d30a808f97d88a64a756e8d15
David Aramant <davidaramant@gmail.com>
2022-09-05 18:15:33 -0400

    Utility to calculate power of 2

---
+++ src/Buddhabrot.Core/Utility.cs
@@ -0,0 +1,16 @@
+﻿namespace Buddhabrot.Core;
+
+public static class Utility
+{
+    public static int GetLargestPowerOfTwoLessThan(int max)
+    {
+        int pow = 0;
+
+        while ((2 << pow) <= max)
+        {
+            pow++;
+        }
+        
+        return pow;
+    }
+}

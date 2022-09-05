fd921600e0e4d700f4c95597e1727b409d525775
David Aramant <davidaramant@gmail.com>
2022-09-05 18:00:06 -0400

    SquareBoundary

---
+++ src/Buddhabrot.Core.Tests/Boundary/SquareBoundaryTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using Buddhabrot.Core.Boundary;
+
+namespace Buddhabrot.Core.Tests.Boundary;
+
+public class SquareBoundaryTests
+{
+    [Fact]
+    public void ShouldGetExpectedQuadrants()
+    {
+        var sq = new SquareBoundary(Point.Empty, 1);
+
+        sq.GetNWQuadrant().Should().Be(new SquareBoundary(Point.Empty, 0));
+        sq.GetNEQuadrant().Should().Be(new SquareBoundary(new Point(1, 0), 0));
+        sq.GetSEQuadrant().Should().Be(new SquareBoundary(new Point(1, 1), 0));
+        sq.GetSWQuadrant().Should().Be(new SquareBoundary(new Point(0, 1), 0));
+    }
+
+    public static IEnumerable<object[]> IntersectionData()
+    {
+        yield return new object[]
+        {
+            new Rectangle(new Point(10, 10), new Size(10, 10)),
+            Rectangle.Empty
+        };
+        yield return new object[]
+        {
+            new Rectangle(new Point(0, 0), new Size(10, 10)),
+            new Rectangle(new Point(0, 0), new Size(2, 2))
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(IntersectionData))]
+    public void ShouldIntersectWithRectangle(Rectangle rect, Rectangle expected)
+    {
+        var sq = new SquareBoundary(new Point(-2, -2), 2);
+        sq.IntersectWith(rect).Should().Be(expected);
+    }
+}
---
+++ src/Buddhabrot.Core/Boundary/SquareBoundary.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Buddhabrot.Core.Boundary;
+
+public readonly record struct SquareBoundary(
+    Point TopLeft,
+    int Scale)
+{
+    public int X => TopLeft.X;
+    public int Y => TopLeft.Y;
+    public bool IsPoint => Scale == 0;
+    public int QuadrantLength => 1 << (Scale - 1);
+
+    public SquareBoundary GetNWQuadrant() => new(TopLeft, Scale - 1);
+    public SquareBoundary GetNEQuadrant() => new(TopLeft + new Size(QuadrantLength, 0), Scale - 1);
+    public SquareBoundary GetSEQuadrant() => new(TopLeft + new Size(QuadrantLength, QuadrantLength), Scale - 1);
+    public SquareBoundary GetSWQuadrant() => new(TopLeft + new Size(0, QuadrantLength), Scale - 1);
+
+    public Rectangle IntersectWith(Rectangle rect)
+    {
+        int length = 1 << Scale;
+
+        int x1 = Math.Max(rect.X, X);
+        int x2 = Math.Min(rect.X + rect.Width, X + length);
+        int y1 = Math.Max(rect.Y, Y);
+        int y2 = Math.Min(rect.Y + rect.Height, Y + length);
+
+        if (x2 >= x1 && y2 >= y1)
+        {
+            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+        }
+
+        return Rectangle.Empty;
+    }
+
+    public override string ToString() => $"{TopLeft}, SideLength = {1 << Scale}";
+}

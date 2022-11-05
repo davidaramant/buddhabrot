e1c89e447e2183d9333d023dec6159beb16ccb43
David Aramant <davidaramant@gmail.com>
2022-11-05 12:46:19 -0400

    Put in some placeholders for visualizations needed for presentation

---
+++ src/Buddhabrot.ManualVisualizations/BoundaryScanningProcessVisualization.cs
@@ -0,0 +1,7 @@
+namespace Buddhabrot.ManualVisualizations;
+
+[Explicit]
+public class BoundaryScanningProcessVisualization : BaseVisualization
+{
+    // TODO: Render a sequence of images that shows how the boundary scanning works
+}
---
+++ src/Buddhabrot.ManualVisualizations/TrajectoryExplanationVisualization.cs
@@ -0,0 +1,8 @@
+namespace Buddhabrot.ManualVisualizations;
+
+[Explicit]
+public class TrajectoryExplanationVisualization : BaseVisualization
+{
+    // TODO: render images of the trajectory of a point
+    // a good trajectory should probably be like 10 jumps
+}

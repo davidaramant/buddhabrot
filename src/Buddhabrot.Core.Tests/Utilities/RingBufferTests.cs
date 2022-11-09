fd95d6d9d17c183c48ae94cfe61fcde83441d4cf
David Aramant <davidaramant@gmail.com>
2022-11-09 09:59:34 -0500

    A RingBuffer class

---
+++ src/Buddhabrot.Core.Tests/Utilities/RingBufferTests.cs
@@ -0,0 +1,31 @@
+﻿using Buddhabrot.Core.Utilities;
+
+namespace Buddhabrot.Core.Tests.Utilities;
+
+public class RingBufferTests
+{
+    [Fact]
+    public void ShouldAddUpToCapacity()
+    {
+        const int capacity = 5;
+        var ring = new RingBuffer<int>(capacity);
+
+        for (int i = 0; i < 10; i++)
+        {
+            ring.Count.Should().Be(Math.Min(i, capacity));
+            ring.Add(i);
+        }
+    }
+
+    [Fact]
+    public void ShouldReplaceLastValue()
+    {
+        var ring = new RingBuffer<int>(3) { 1, 2, 3 };
+
+        ring.Should().BeEquivalentTo(new[] { 1, 2, 3 });
+
+        ring.Add(4);
+
+        ring.Should().BeEquivalentTo(new[] { 2, 3, 4 });
+    }
+}
---
+++ src/Buddhabrot.Core/Utilities/RingBuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buddhabrot.Core.Utilities;
+
+public sealed class RingBuffer<T> : IEnumerable<T>
+{
+    private readonly T[] _buffer;
+    private int _start;
+    public int Capacity => _buffer.Length;
+    public int Count { get; private set; }
+
+    public RingBuffer(int capacity)
+    {
+        _buffer = new T[capacity];
+    }
+
+    public void Add(T item)
+    {
+        var nextIndex = (_start + Count + 1) % Capacity;
+        _buffer[nextIndex] = item;
+
+        Count = Math.Min(Count + 1, Capacity);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for(int i = 0; i < Count; i++)
+        {
+            var index = (i + _start) % Capacity;
+            yield return _buffer[index];    
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}

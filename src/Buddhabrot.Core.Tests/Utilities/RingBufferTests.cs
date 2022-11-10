143c65e422b9e8c1aef198f11d15b241f77660bc
David Aramant <davidaramant@gmail.com>
2022-11-09 21:27:51 -0500

    Fixed ring buffer

--- src/Buddhabrot.Core.Tests/Utilities/RingBufferTests.cs
+++ src/Buddhabrot.Core.Tests/Utilities/RingBufferTests.cs
@@ -20,12 +20,24 @@ public class RingBufferTests
     [Fact]
     public void ShouldReplaceLastValue()
     {
-        var ring = new RingBuffer<int>(3) { 1, 2, 3 };
+        var ring = new RingBuffer<int>(3) { 1 };
 
+        ring.Should().BeEquivalentTo(new[] { 1 });
+
+        ring.Add(2);
+
+        ring.Should().BeEquivalentTo(new[] { 1, 2 });
+        
+        ring.Add(3);
+        
         ring.Should().BeEquivalentTo(new[] { 1, 2, 3 });
 
         ring.Add(4);
 
         ring.Should().BeEquivalentTo(new[] { 2, 3, 4 });
+        
+        ring.Add(5);
+        
+        ring.Should().BeEquivalentTo(new[] { 3, 4, 5 });
     }
 }
--- src/Buddhabrot.Core/Utilities/RingBuffer.cs
+++ src/Buddhabrot.Core/Utilities/RingBuffer.cs
@@ -10,7 +10,8 @@ namespace Buddhabrot.Core.Utilities;
 public sealed class RingBuffer<T> : IEnumerable<T>
 {
     private readonly T[] _buffer;
-    private int _start;
+    private int _head = 0;
+    private int _tail = 0;
     public int Capacity => _buffer.Length;
     public int Count { get; private set; }
 
@@ -21,17 +22,31 @@ public sealed class RingBuffer<T> : IEnumerable<T>
 
     public void Add(T item)
     {
-        var nextIndex = (_start + Count + 1) % Capacity;
-        _buffer[nextIndex] = item;
+        _buffer[_tail] = item;
 
+        _tail = (_tail + 1) % Capacity;
+
+        if (Count == Capacity)
+        {
+            _head = (_head + 1) % Capacity;
+        }
+        
         Count = Math.Min(Count + 1, Capacity);
+    }
+
+    public void AddRange(IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            Add(item);
+        }
     }
 
     public IEnumerator<T> GetEnumerator()
     {
         for(int i = 0; i < Count; i++)
         {
-            var index = (i + _start) % Capacity;
+            var index = (i + _head) % Capacity;
             yield return _buffer[index];    
         }
     }

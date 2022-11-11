5972c8a6074fbb0fe927662fa8175c74a56c90d4
David Aramant <davidaramant@gmail.com>
2022-11-10 21:28:28 -0500

    boundary scanning visualization updates

--- src/Buddhabrot.ManualVisualizations/BoundaryScanningProcess.cs
+++ src/Buddhabrot.ManualVisualizations/BoundaryScanningProcess.cs
@@ -1,8 +1,9 @@
 using System.Drawing;
+using System.Linq;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Buddhabrot.Core.Boundary;
 using Buddhabrot.Core.Images;
+using Buddhabrot.Core.Utilities;
 
 namespace Buddhabrot.ManualVisualizations;
 
@@ -22,9 +23,88 @@ public class BoundaryScanningProcess : BaseVisualization
     public readonly record struct Step(int X, int Y, RegionType Type);
 
     [Test]
-    public void CreateSession()
+    public void CreateAndSaveSession()
     {
-        var parameters = new BoundaryParameters(new AreaDivisions(VerticalPower: 8), MaxIterations: 15_000_000);
+        var session = CreateSession();
+
+        using var fs = File.Open(SessionFilePath, FileMode.Create);
+        JsonSerializer.Serialize(fs, session);
+    }
+
+    [Test]
+    public void GenerateFrames()
+    {
+        foreach (var png in OutputPath.GetFiles("*.png"))
+        {
+            png.Delete();
+        }
+
+        var session = CreateSession();
+
+        var palette = new Dictionary<RegionType, Color>
+        {
+            { RegionType.Empty, Color.LightGray },
+            { RegionType.Border, Color.DarkSlateGray },
+            { RegionType.Filament, Color.Gray },
+            { RegionType.InSet, Color.SlateGray },
+        };
+
+        using var image = new RasterImage(session.MaxX + 1, session.MaxY + 1, scale: 2);
+        image.Fill(Color.White);
+
+        var fire = new[]
+        {
+            Color.Brown,
+            Color.DarkRed,
+            Color.Red,
+            Color.OrangeRed,
+            Color.Orange,
+        };
+
+        var buffer = new RingBuffer<Step>(fire.Length * 2);
+
+        int frame = 0;
+
+        SaveImage(image, $"frame{frame++:00000}");
+
+        foreach (var batch in session.Steps.Chunk(fire.Length))
+        {
+            buffer.AddRange(batch);
+
+            int index = 0;
+            foreach (var step in buffer)
+            {
+                var color = index >= fire.Length ? fire[index - fire.Length] : palette[step.Type];
+
+                image.SetPixel(step.X, (session.MaxY - step.Y), color);
+
+                index++;
+            }
+
+            SaveImage(image, $"frame{frame++:00000}");
+        }
+
+        foreach (var step in buffer)
+        {
+            var color = palette[step.Type];
+
+            image.SetPixel(step.X, (session.MaxY - step.Y), color);
+        }
+
+        SaveImage(image, $"frame{frame++:00000}");
+        
+        // ffmpeg -framerate 60 -i frame%05d.png -pix_fmt yuv420p -y output.mp4 && open output.mp4
+    }
+
+    private ScanSession LoadSession()
+    {
+        using var fs = File.OpenRead(SessionFilePath);
+        return JsonSerializer.Deserialize<ScanSession>(fs) ?? throw new InvalidDataException();
+    }
+
+    private ScanSession CreateSession()
+    {
+        var parameters = new BoundaryParameters(new AreaDivisions(VerticalPower: 9), MaxIterations: 15_000_000);
         var steps = new List<Step>();
         int maxX = 0;
         int maxY = 0;
@@ -36,36 +116,9 @@ public class BoundaryScanningProcess : BaseVisualization
                 steps.Add(new Step(regionId.X, regionId.Y, type));
             });
 
-        var session = new ScanSession(
+        return new ScanSession(
             maxX,
             maxY,
             steps);
-
-        using var fs = File.Open(SessionFilePath, FileMode.Create);
-        JsonSerializer.Serialize(fs, session);
-    }
-
-    [Test]
-    public void GenerateFrames()
-    {
-        var session = LoadSession();
-
-        using var image = new RasterImage(session.MaxX + 1, session.MaxY + 1, scale: 4);
-        image.Fill(Color.White);
-
-        for (int frame = 0; frame < session.Steps.Count; frame++)
-        {
-            var step = session.Steps[frame];
-
-            image.SetPixel(step.X, (session.MaxY - step.Y), Color.Black);
-            SaveImage(image, $"frame{frame:00000}");
-        }
-        // ffmpeg -framerate 60 -i frame%05d.png -pix_fmt yuv420p output.mp4
-    }
-
-    private ScanSession LoadSession()
-    {
-        using var fs = File.OpenRead(SessionFilePath);
-        return JsonSerializer.Deserialize<ScanSession>(fs) ?? throw new InvalidDataException();
     }
 }

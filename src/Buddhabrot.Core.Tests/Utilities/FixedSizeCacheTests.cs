db11f92804ea937efa844a1ee681ad56cb1e8cab
David Aramant <davidaramant@gmail.com>
2022-11-14 13:25:52 -0500

    FixedSizeCache class

---
+++ src/Buddhabrot.Core.Tests/Utilities/FixedSizeCacheTests.cs
@@ -0,0 +1,34 @@
+﻿using Buddhabrot.Core.Utilities;
+
+namespace Buddhabrot.Core.Tests.Utilities;
+
+public class FixedSizeCacheTests
+{
+    private readonly FixedSizeCache<int, int> _cache = new(4);
+
+    [Fact]
+    public void ShouldHandleEntriesNotInCache()
+    {
+        _cache.TryGetValue(0, out _).Should().BeFalse();
+    }
+
+    [Fact]
+    public void ShouldLookUpAddedEntry()
+    {
+        _cache.Add(1, 2);
+
+        _cache.TryGetValue(1, out var value).Should().BeTrue();
+        value.Should().Be(2);
+    }
+
+    [Fact]
+    public void ShouldRollOverContents()
+    {
+        for (int i = 0; i < _cache.Capacity + 1; i++)
+        {
+            _cache.Add(i, i);
+        }
+
+        _cache.TryGetValue(0, out _).Should().BeFalse();
+    }
+}
---
+++ src/Buddhabrot.Core/Utilities/FixedSizeCache.cs
@@ -0,0 +1,40 @@
+﻿namespace Buddhabrot.Core.Utilities;
+
+public sealed class FixedSizeCache<TKey, TValue> where TKey : struct where TValue : struct
+{
+    private readonly RingBuffer<(TKey Key, TValue Value)> _cache;
+
+    public int Capacity => _cache.Capacity;
+    public int Count => _cache.Count;
+
+    public FixedSizeCache(int maxSize) => _cache = new(maxSize);
+
+    public void Add(TKey key, TValue value)
+    {
+        foreach (var item in _cache)
+        {
+            if (Equals(item.Key, key))
+            {
+                return;
+            }
+        }
+
+        _cache.Add((key, value));
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        foreach (var item in _cache)
+        {
+            if (item.Key.Equals(key))
+            {
+                value = item.Value;
+                return true;
+            }
+        }
+
+        value = default;
+
+        return false;
+    }
+}

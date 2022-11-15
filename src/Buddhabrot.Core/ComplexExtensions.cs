07ddc7be73964fd21d3311d4eccc02883d460429
David Aramant <davidaramant@gmail.com>
2022-11-14 22:34:25 -0500

    Useless inline hint for MagnitudeSquared

--- src/Buddhabrot.Core/ComplexExtensions.cs
+++ src/Buddhabrot.Core/ComplexExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Buddhabrot.Core;
 
 public static class ComplexExtensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double MagnitudeSquared(this Complex c) => c.Real * c.Real + c.Imaginary * c.Imaginary;
 }

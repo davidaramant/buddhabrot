e0ba079360957c9a82063cd8c4e279636f572f25
David Aramant <davidaramant@gmail.com>
2022-12-25 12:08:23 -0500

    Condensing region types

--- src/Buddhabrot.Core/Boundary/RegionLookupDiffer.cs
+++ src/Buddhabrot.Core/Boundary/RegionLookupDiffer.cs
@@ -1,23 +1,115 @@
-﻿namespace Buddhabrot.Core.Boundary;
-using Region = Buddhabrot.Core.Boundary.LookupRegionType;
+﻿using System.Numerics;
+using Buddhabrot.Core.Boundary.QuadTrees;
+
+namespace Buddhabrot.Core.Boundary;
+
+using Type = LookupRegionType;
 
 public static class RegionLookupDiffer
 {
-    private static readonly IReadOnlyDictionary<(Region Left, Region Right), Region>
-        TypeMappings = new Dictionary<(Region Left, Region Right), Region>
+    private static readonly IReadOnlyDictionary<(Type Left, Type Right), Type>
+        TypeMappings = new Dictionary<(Type Left, Type Right), Type>
         {
-            {(Region.Empty, Region.Empty), Region.Empty},
-            {(Region.Empty, Region.EmptyToBorder), Region.EmptyToBorder},
-            {(Region.Empty, Region.EmptyToFilament), Region.EmptyToFilament},
-            
-            {(Region.EmptyToBorder, Region.Empty), Region.BorderToEmpty},
-            {(Region.EmptyToBorder, Region.EmptyToBorder), Region.Empty},
-            {(Region.EmptyToBorder, Region.EmptyToFilament), Region.BorderToFilament},
+            {(Type.Empty, Type.Empty), Type.Empty},
+            {(Type.Empty, Type.EmptyToBorder), Type.EmptyToBorder},
+            {(Type.Empty, Type.EmptyToFilament), Type.EmptyToFilament},
 
-            {(Region.EmptyToFilament, Region.Empty), Region.Empty},
-            {(Region.EmptyToFilament, Region.EmptyToBorder), Region.FilamentToBorder},
-            {(Region.EmptyToFilament, Region.EmptyToFilament), Region.Empty},
+            {(Type.EmptyToBorder, Type.Empty), Type.BorderToEmpty},
+            {(Type.EmptyToBorder, Type.EmptyToBorder), Type.Empty},
+            {(Type.EmptyToBorder, Type.EmptyToFilament), Type.BorderToFilament},
+
+            {(Type.EmptyToFilament, Type.Empty), Type.Empty},
+            {(Type.EmptyToFilament, Type.EmptyToBorder), Type.FilamentToBorder},
+            {(Type.EmptyToFilament, Type.EmptyToFilament), Type.Empty},
         };
 
-    public static Region DetermineType(Region left, Region right) => TypeMappings[(left, right)];
+    public static RegionLookup Diff(RegionLookup left, RegionLookup right)
+    {
+        var height = Math.Max(left.Height, right.Height);
+
+        var nodes = new List<RegionNode>();
+        
+        // TODO: Do diff
+        
+        return new RegionLookup(nodes, height);
+    }
+    
+    public static Type DetermineType(Type left, Type right) => TypeMappings[(left, right)];
+
+    public static Type CondenseRegionType(RegionNode sw, RegionNode se, RegionNode nw, RegionNode ne) =>
+        CondenseRegionType(sw.RegionType, se.RegionType, nw.RegionType, ne.RegionType);
+
+    public static Type CondenseRegionType(
+        Type sw,
+        Type se,
+        Type nw,
+        Type ne)
+    {
+        uint groups = 0;
+        int emptyToBorderCount = 0;
+        int emptyToFilamentCount = 0;
+        int borderToEmptyCount = 0;
+        int filamentToEmptyCount = 0;
+
+        Count(sw);
+        Count(se);
+        Count(nw);
+        Count(ne);
+
+        var numGroups = BitOperations.PopCount(groups);
+        if (numGroups == 0)
+            return Type.Empty;
+
+        if (numGroups > 1)
+            return Type.MixedDiff;
+
+        if((groups & 0b1) == 0b1)
+            return emptyToBorderCount >= emptyToFilamentCount ? Type.EmptyToBorder : Type.EmptyToFilament;
+
+        if((groups & 0b10) == 0b10)
+            return borderToEmptyCount >= filamentToEmptyCount ? Type.BorderToEmpty : Type.FilamentToEmpty;
+
+        return (groups & 0b100) == 0b100 ? Type.BorderToFilament : Type.FilamentToBorder;
+
+        void Count(Type type)
+        {
+            switch (type)
+            {
+                case Type.Empty:
+                    break;
+
+                case Type.EmptyToBorder:
+                    groups |= 0b1;
+                    emptyToBorderCount++;
+                    break;
+
+                case Type.EmptyToFilament:
+                    groups |= 0b1;
+                    emptyToFilamentCount++;
+                    break;
+
+                case Type.BorderToEmpty:
+                    groups |= 0b10;
+                    borderToEmptyCount++;
+                    break;
+                
+                case Type.FilamentToEmpty:
+                    groups |= 0b10;
+                    filamentToEmptyCount++;
+                    break;
+                
+                case Type.BorderToFilament:
+                    groups |= 0b100;
+                    break;
+                
+                case Type.FilamentToBorder:
+                    groups |= 0b1000;
+                    break;
+                
+                case Type.MixedDiff:
+                    groups = 0b1111;
+                    break;
+            }
+        }
+    }
 }

df59a2a4d7a9378ef27256e023795e8e09421505
David Aramant <davidaramant@gmail.com>
2022-12-25 14:14:35 -0500

    WIP on support diffs on file system

--- src/Buddhabrot.Core/Boundary/BoundaryParameters.cs
+++ src/Buddhabrot.Core/Boundary/BoundaryParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Text.RegularExpressions;
+using Humanizer;
 
 namespace Buddhabrot.Core.Boundary;
 
@@ -10,14 +11,14 @@ public sealed record BoundaryParameters(
     public override string ToString() =>
         $"({Divisions.VerticalPower}) Vertical Divisions: {Divisions.QuadrantDivisions:N0}, Max Iterations: {MaxIterations:N0}";
 
-    public string Description => $"v{Divisions.VerticalPower:N0}_i{MaxIterations:N0}";
+    public string Description => $"v{Divisions.VerticalPower:N0}_i{MaxIterations.ToMetric()}";
 
     public static BoundaryParameters FromDescription(string description)
     {
-        var descriptionRegex = new Regex(@"v([\d,]+)_i([\d\,]+)", RegexOptions.Compiled);
+        var descriptionRegex = new Regex(@"v([\d,]+)_i(.+)", RegexOptions.Compiled);
         var match = descriptionRegex.Match(description);
         return new BoundaryParameters(
             Divisions: new AreaDivisions(int.Parse(match.Groups[1].Value, NumberStyles.AllowThousands)),
-            MaxIterations: int.Parse(match.Groups[2].Value, NumberStyles.AllowThousands));
+            MaxIterations: (int)match.Groups[2].Value.FromMetric());
     }
 }
---
+++ src/Buddhabrot.Core/DataStorage/BoundaryDataSet.cs
@@ -0,0 +1,26 @@
+﻿using Buddhabrot.Core.Boundary;
+using Humanizer;
+
+namespace Buddhabrot.Core.DataStorage;
+
+public sealed record BoundaryDataSet(
+    bool IsDiff,
+    BoundaryParameters Parameters,
+    string Description)
+{
+    public override string ToString() => Description;
+
+    public static BoundaryDataSet FromBoundary(BoundaryParameters parameters) =>
+        new(IsDiff: false, parameters, parameters.Description);
+
+    // TODO: Need easy way to condense iterations
+    public static BoundaryDataSet FromDiff(BoundaryParameters left, BoundaryParameters right) =>
+        new(
+            IsDiff: true, 
+            Combine(left, right),
+            Description: $"Diff - v{left.Divisions.VerticalPower}i{left.MaxIterations.ToMetric()} - v{right.Divisions.VerticalPower}i{right.MaxIterations.ToMetric()}");
+
+    private static BoundaryParameters Combine(BoundaryParameters one, BoundaryParameters two) => new(
+        new AreaDivisions(Math.Max(one.Divisions.VerticalPower, two.Divisions.VerticalPower)),
+        Math.Max(one.MaxIterations, two.MaxIterations));
+}

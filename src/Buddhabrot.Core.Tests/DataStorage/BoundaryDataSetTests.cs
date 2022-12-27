36862de7343c04e2acf7f7c548c5c96ce0d1fc91
David Aramant <davidaramant@gmail.com>
2022-12-27 12:28:20 -0500

    Tests for boundary data set descriptions

---
+++ src/Buddhabrot.Core.Tests/DataStorage/BoundaryDataSetTests.cs
@@ -0,0 +1,24 @@
+﻿using Buddhabrot.Core.DataStorage;
+
+namespace Buddhabrot.Core.Tests.DataStorage;
+
+public class BoundaryDataSetTests
+{
+    [Fact]
+    public void ShouldDeserializeNormalBoundaryParameters()
+    {
+        var bds = BoundaryDataSet.FromDescription("v8_i1M");
+        bds.IsDiff.Should().BeFalse();
+        bds.Parameters.Divisions.VerticalPower.Should().Be(8);
+        bds.Parameters.MaxIterations.Should().Be(1_000_000);
+    }
+    
+    [Fact]
+    public void ShouldDeserializeDiff()
+    {
+        var bds = BoundaryDataSet.FromDescription("Diff - v9_i1M - v8_i100k");
+        bds.IsDiff.Should().BeTrue();
+        bds.Parameters.Divisions.VerticalPower.Should().Be(9);
+        bds.Parameters.MaxIterations.Should().Be(1_000_000);
+    }
+}
--- src/Buddhabrot.Core/DataStorage/BoundaryDataSet.cs
+++ src/Buddhabrot.Core/DataStorage/BoundaryDataSet.cs
@@ -1,5 +1,4 @@
 ﻿using Buddhabrot.Core.Boundary;
-using Humanizer;
 
 namespace Buddhabrot.Core.DataStorage;
 
@@ -13,14 +12,28 @@ public sealed record BoundaryDataSet(
     public static BoundaryDataSet FromBoundary(BoundaryParameters parameters) =>
         new(IsDiff: false, parameters, parameters.Description);
 
-    // TODO: Need easy way to condense iterations
     public static BoundaryDataSet FromDiff(BoundaryParameters left, BoundaryParameters right) =>
         new(
-            IsDiff: true, 
+            IsDiff: true,
             Combine(left, right),
-            Description: $"Diff - v{left.Divisions.VerticalPower}i{left.MaxIterations.ToMetric()} - v{right.Divisions.VerticalPower}i{right.MaxIterations.ToMetric()}");
+            Description: $"Diff - {left.Description} - {right.Description}");
 
     private static BoundaryParameters Combine(BoundaryParameters one, BoundaryParameters two) => new(
         new AreaDivisions(Math.Max(one.Divisions.VerticalPower, two.Divisions.VerticalPower)),
         Math.Max(one.MaxIterations, two.MaxIterations));
+
+    public static BoundaryDataSet FromDescription(string description)
+    {
+        if (description.StartsWith("Diff"))
+        {
+            var parts = description.Split("-");
+            return FromDiff(
+                BoundaryParameters.FromDescription(parts[1].Trim()),
+                BoundaryParameters.FromDescription(parts[2].Trim()));
+        }
+        else
+        {
+            return FromBoundary(BoundaryParameters.FromDescription(description));
+        }
+    }
 }

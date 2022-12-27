d746d7648504f855dffa839728387b007734d7e6
David Aramant <davidaramant@gmail.com>
2022-12-27 12:21:11 -0500

    Tests for boundary parameter parsing

---
+++ src/Buddhabrot.Core.Tests/Boundary/BoundaryParametersTests.cs
@@ -0,0 +1,24 @@
+﻿using Buddhabrot.Core.Boundary;
+
+namespace Buddhabrot.Core.Tests.Boundary;
+
+public class BoundaryParametersTests
+{
+    [Theory]
+    [InlineData(1_000)]
+    [InlineData(1_000_000)]
+    public void ShouldRoundtrip(int iterations)
+    {
+        var bp = new BoundaryParameters(new AreaDivisions(10), iterations);
+        var roundTripped = BoundaryParameters.FromDescription(bp.Description);
+        roundTripped.MaxIterations.Should().Be(iterations);
+    }
+
+    [Fact]
+    public void ShouldDeserializeFromOldFormat()
+    {
+        var bp = BoundaryParameters.FromDescription("v8_i1,000,000");
+        bp.Divisions.VerticalPower.Should().Be(8);
+        bp.MaxIterations.Should().Be(1_000_000);
+    }
+}

88b982dfe8254fe38e060bf79a8151cc2f474fb2
David Aramant <davidaramant@gmail.com>
2022-12-30 22:38:56 -0500

    RLE list for region types

---
+++ src/BoundaryFinder.Tests/Views/LookupRegionTypeListTests.cs
@@ -0,0 +1,37 @@
+﻿using BoundaryFinder.Gui.Views;
+using Buddhabrot.Core.Boundary;
+
+namespace BoundaryFinder.Tests.Views;
+
+public class LookupRegionTypeListTests
+{
+    [Fact]
+    public void ShouldReturnCorrectTypesForMultipleRuns()
+    {
+        var list = new LookupRegionTypeList();
+        list.Add(LookupRegionType.MixedDiff, 2);
+        list.Add(LookupRegionType.MixedDiff, 3);
+
+        for (int i = 0; i < 5; i++)
+        {
+            list.Consume().Should().Be(LookupRegionType.MixedDiff);
+        }
+    }
+
+    [Fact]
+    public void ShouldReturnCorrectTypesForMultipleTypes()
+    {
+        var list = new LookupRegionTypeList();
+        list.Add(LookupRegionType.MixedDiff, 2);
+        list.Add(LookupRegionType.EmptyToBorder, 3);
+
+        for (int i = 0; i < 2; i++)
+        {
+            list.Consume().Should().Be(LookupRegionType.MixedDiff);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            list.Consume().Should().Be(LookupRegionType.EmptyToBorder);
+        }
+    }
+}

38ea42350dd1715b9a3d2bcd7e2bf2d8f9a25b68
David Aramant <davidaramant@gmail.com>
2024-03-15 08:59:22 -0400

    Switched to using IntPtr instead of raw pointers in AlignedArrayPool

--- src/Buddhabrot.Core/Utilities/AlignedArrayPool.cs
+++ src/Buddhabrot.Core/Utilities/AlignedArrayPool.cs
@@ -5,19 +5,21 @@ namespace Buddhabrot.Core.Utilities;
 
 public sealed unsafe class AlignedArrayPool : IDisposable
 {
-	private readonly ConcurrentBag<ulong> _pool = new();
+	private readonly ConcurrentBag<IntPtr> _pool = new();
 
 	public const int ArrayBytes = 4096;
+	public int Count => _pool.Count;
 
-	public void* Rent() => _pool.TryTake(out var array) ? (void*)array : NativeMemory.AlignedAlloc(ArrayBytes, 64);
+	public IntPtr Rent() =>
+		_pool.TryTake(out var array) ? array : new IntPtr(NativeMemory.AlignedAlloc(ArrayBytes, 64));
 
-	public void Return(void* array) => _pool.Add((ulong)array);
+	public void Return(IntPtr array) => _pool.Add(array);
 
 	public void Dispose()
 	{
 		foreach (var array in _pool)
 		{
-			NativeMemory.Free((void*)array);
+			NativeMemory.Free(array.ToPointer());
 		}
 	}
 }

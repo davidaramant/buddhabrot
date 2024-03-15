1ba7f7b1329e1db6acc600b1a767262b63086f6c
David Aramant <davidaramant@gmail.com>
2024-03-15 09:05:19 -0400

    Added a constant & comment to AlignedArrayPool

--- src/Buddhabrot.Core/Utilities/AlignedArrayPool.cs
+++ src/Buddhabrot.Core/Utilities/AlignedArrayPool.cs
@@ -7,11 +7,15 @@ public sealed unsafe class AlignedArrayPool : IDisposable
 {
 	private readonly ConcurrentBag<IntPtr> _pool = new();
 
+	/// <summary>
+	/// I think AVX2 would be fine with 32 (256 bits / 8) but this will also work for AVX512
+	/// </summary>
+	private const int Alignment = 64;
 	public const int ArrayBytes = 4096;
 	public int Count => _pool.Count;
 
 	public IntPtr Rent() =>
-		_pool.TryTake(out var array) ? array : new IntPtr(NativeMemory.AlignedAlloc(ArrayBytes, 64));
+		_pool.TryTake(out var array) ? array : new IntPtr(NativeMemory.AlignedAlloc(ArrayBytes, Alignment));
 
 	public void Return(IntPtr array) => _pool.Add(array);
 

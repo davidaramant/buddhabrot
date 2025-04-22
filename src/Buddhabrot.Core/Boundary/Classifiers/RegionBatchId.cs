27269d95392c2bae2936a0ad2fa303c42be615aa
David Aramant <davidaramant@gmail.com>
2025-04-21 22:57:00 -0400

    Minor tweak to RegionBatchId

--- src/Buddhabrot.Core/Boundary/Classifiers/RegionBatchId.cs
+++ src/Buddhabrot.Core/Boundary/Classifiers/RegionBatchId.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Buddhabrot.Core.Utilities;
 
 namespace Buddhabrot.Core.Boundary.Classifiers;
@@ -13,15 +14,21 @@ public readonly record struct RegionBatchId(int X, int Y)
 
 	public static readonly RegionBatchId Invalid = new(int.MaxValue, int.MaxValue);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public CornerId GetBottomLeftCorner() => new(X * CornerWidth, Y * CornerWidth);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public RegionId GetBottomLeftRegion() => new(X * RegionWidth, Y * RegionWidth);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetHashCode16() => XYHash.GetHashCode16(X, Y);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetHashCode64() => XYHash.GetHashCode64(X, Y);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetHashCode256() => XYHash.GetHashCode256(X, Y);
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public int GetHashCode1024() => XYHash.GetHashCode1024(X, Y);
 }
